f6aedfd8143c5d4215379253feda5f0f8817ac84
Anubhav-MI <120900218+Anubhav-MI@users.noreply.github.com>
2024-08-31 21:59:45 +0530

    new problem

--- B_Array_merging.cpp
+++ B_Array_merging.cpp
@@ -1,73 +1,56 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main()
+
+int32_t main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int q;
+    cin >> q;
+    while (q--)
     {
         int n;
         cin >> n;
-        vector<long long> a;
-        vector<long long> b;
-        for (int i = 0; i < n; i++)
+        vector<int> a(n + 1);
+        vector<int> b(n + 1);
+        for (int i = 1; i <= n; ++i)
         {
-            long long x;
-            cin >> x;
-            a.push_back(x);
+            cin >> a[i];
         }
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; ++i)
         {
-            long long x;
-            cin >> x;
-            b.push_back(x);
+            cin >> b[i];
         }
-        vector<long long> c;
-        int i = 1;
-        int j = 0;
-        int last = a[0];
-        c.push_back(a[0]);
-        while (i < n || j < n)
+        vector<int> fa(n + n + 1);
+        vector<int> fb(n + n + 1);
+        int p = 1;
+        for (int i = 2; i <= n; ++i)
         {
-            if (i < n && a[i] == last)
+            if (a[i] != a[i - 1])
             {
-                last = a[i];
-                c.push_back(a[i]);
-                i++;
-            }
-            else if (j < n && b[j] == last)
-            {
-                last = b[j];
-                c.push_back(b[j]);
-                j++;
-            }
-            else if (i < n)
-            {
-                last = a[i];
-                c.push_back(a[i]);
-                i++;
-            }
-            else if (j < n)
-            {
-                last = b[j];
-                c.push_back(b[j]);
-                j++;
+                fa[a[i - 1]] = max(fa[a[i - 1]], i - p);
+                p = i;
             }
         }
+        fa[a[n]] = max(fa[a[n]], n - p + 1);
+
+        p = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            if (b[i] != b[i - 1])
+            {
+                fb[b[i - 1]] = max(fb[b[i - 1]], i - p);
+                p = i;
+            }
+        }
+        fb[b[n]] = max(fb[b[n]], n - p + 1);
+
         int ans = 0;
-        int cnt = 1;
-        for (int i = 0; i < (2 * n) - 1; i++)
+        for (int i = 1; i <= n + n; ++i)
         {
-            if (c[i] == c[i + 1])
-                cnt++;
-            else
-            {
-                ans = max(ans, cnt);
-                cnt = 1;
-            }
+            ans = max(ans, fa[i] + fb[i]);
         }
-        ans = max(ans, cnt);
-        cout << ans << endl;
+
+        cout << ans << '\n';
     }
-    return 0;
 }
